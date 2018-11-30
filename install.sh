20bc05941bce986a1dd8c3706c230c5ebadc754a
kali <kali@localhost.localdomain>
2018-11-30 20:53:46 +0330

    update installing

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ mkdir -p /opt/netscanner
 cp -r netscanner.py /opt/netscanner
 cp netscanner /usr/bin
 chmod +x /usr/bin/netscanner
+
+echo '[+] Installed Successfully.'
+echo '[+] Type "netscanner"'
